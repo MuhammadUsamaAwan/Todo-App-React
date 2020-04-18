09a11b2ae5949525f116d3c7444dc927f6833c65
Usama Awan <underusama@gmail.com>
2020-04-19 00:18:43 +0500

    Updates

---
+++ precache-manifest.0d7e78c75d05afaece7c759aad69b755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2675a7b478e537b98460661d16ce7de6",
+    "url": "/Todo-App-React/index.html"
+  },
+  {
+    "revision": "0096f420c6bd567127a5",
+    "url": "/Todo-App-React/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0096f420c6bd567127a5",
+    "url": "/Todo-App-React/static/js/2.5a82d0de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App-React/static/js/2.5a82d0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c169006af1fd9cb560",
+    "url": "/Todo-App-React/static/js/main.e8914f83.chunk.js"
+  },
+  {
+    "revision": "dbb027b6beb037a1da55",
+    "url": "/Todo-App-React/static/js/runtime-main.d31fb957.js"
+  }
+]);
